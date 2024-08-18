146958ad98fa47d583e68fadd036e85790f699d4
Aline Leroy <aline.leroy.27@gmail.com>
2024-08-18 23:12:12 +0200

    add adapter to deploy netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  output: "server",
+  adapter: netlify()
 });
