4beeba0bd0c0d2e1281e9963eceb1416768ef7a0
ludoguenet <ludovicguenet@gmx.com>
2024-08-22 22:33:22 +0200

    feat: added OpenTechCon

---
+++ src/datas/conferences/OpenTechCon/OpenTechConconferences.ts
@@ -0,0 +1,87 @@
+export const OpenTechConConferences = [
+    {
+        "title": "Ouverture de l'événement",
+        "description": "Par Yoan Bernabeu",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Keynote d'ouverture",
+        "description": "Par Marcy Ericka Charollois",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Session 1 - Talk",
+        "description": "Par Paola Mauceri",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Interlude",
+        "description": "",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Session 2 - Talk",
+        "description": "Par Loreleï Moutiez",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Pause Sponsor",
+        "description": "Par Yoan Bernabeu",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Session 3 - Ignite Talks",
+        "description": "Par 2 sur 3 programmés",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Clôture de la matinée",
+        "description": "Par Yoan Bernabeu",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Pause Déjeuner",
+        "description": "",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Session 4 - Main Talk",
+        "description": "Par Smaïne Milianni",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Interlude",
+        "description": "",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Session 5 - Talk",
+        "description": "Par Pest soit de Laravel",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Pause Sponsor",
+        "description": "Par Yoan Bernabeu",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Session 6 - Ignite Talks",
+        "description": "Par En cours de programmation",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Interlude",
+        "description": "",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Session 7 - Demo Technique",
+        "description": "Par Hidora",
+        "path": "https://opentechcon.fr/programme"
+    },
+    {
+        "title": "Clôture de l'événement",
+        "description": "Par Yoan Bernabeu",
+        "path": "https://opentechcon.fr/programme"
+    }
+]
