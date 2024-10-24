8889bf8d202142c8933cd9519a3cc2aa250153cd
shelbon <20593460+shelbon@users.noreply.github.com>
2024-10-24 22:03:24 +0200

    add to crafting section xtrem tdd and best practice item

--- src/datas/skills/craftingdatas.ts
+++ src/datas/skills/craftingdatas.ts
@@ -1,7 +1,17 @@
 export const craftingdatas = [
-    {
-        title: 'Understand Legacy Code',
-        description: 'Blog de Nicolas Carlo apportant des conseils pour comprendre et travailler sur du code dit "legacy".',
-        path:  'https://understandlegacycode.com/'
-    },
+  {
+    title: 'Understand Legacy Code',
+    description: 'Blog de Nicolas Carlo apportant des conseils pour comprendre et travailler sur du code dit "legacy".',
+    path: 'https://understandlegacycode.com/'
+  },
+  {
+    title: 'Xtrem T.D.D',
+    description: "Base de connaissances par 'Les Tonton Crafters' regroupant des solutions et ressources aux défis fréquents de l'ingénierie logicielle, fondée sur une expertise collective et des décennies d'expérience pour accompagner les développeurs dans leur progression continue",
+    path: 'https://xtrem-tdd.netlify.app/'
+  },
+  {
+    title: 'Best practices',
+    description: "Un ensemble de bonne pratique sur un éventail large  de sujet,  architecture, accessibilité...",
+    path: 'https://bestcodingpractices.dev/'
+  },
 ];
