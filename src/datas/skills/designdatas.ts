68b8737e3088ad90b636676151a97031462a9948
frontcodelover <contact@avantjetaisriche.com>
2024-11-04 13:29:13 +0100

    Add flexbox froggy and Grid Garden

--- src/datas/skills/designdatas.ts
+++ src/datas/skills/designdatas.ts
@@ -1,7 +1,17 @@
 export const designdatas = [
-    {
-        title: '100 Days Daily UI Challenge',
-        description: 'Un défi pour chaque jour afin de s\'améliorer en design UI. Un bon moyen de s\'entraîner et de se challenger !',
-        path:  'https://www.figma.com/community/file/1282661432742080471/100-days-daily-ui-challenge'
-    },
+  {
+    title: '100 Days Daily UI Challenge',
+    description: "Un défi pour chaque jour afin de s'améliorer en design UI. Un bon moyen de s'entraîner et de se challenger !",
+    path: 'https://www.figma.com/community/file/1282661432742080471/100-days-daily-ui-challenge',
+  },
+  {
+    title: 'Flexbox Froggy',
+    description: 'Flexbox Froggy est un jeu qui vous apprend à utiliser Flexbox pour organiser vos éléments sur une page.',
+    path: 'https://flexboxfroggy.com/#fr',
+  },
+  {
+    title: 'Grid Garden',
+    description: 'Grid Garden est un jeu qui vous apprend à utiliser CSS Grid pour organiser vos éléments sur une page.',
+    path: 'https://cssgridgarden.com/#fr',
+  },
 ];
