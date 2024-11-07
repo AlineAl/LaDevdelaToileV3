74f602f3f2e4e93f262820472950bc0b29110d17
Mickael Suard <suard.mickael72@gmail.com>
2024-11-07 17:41:30 +0100

    add host

--- src/datas/skills/hostdatas.ts
+++ src/datas/skills/hostdatas.ts
@@ -4,4 +4,14 @@ export const hostdatas = [
         description: 'Cours proposé par Grafikart pour apprendre à mettre son site en ligne.',
         path:  'https://grafikart.fr/formations/upload-site'
     },
+    {
+        title: 'OVH',
+        description: "Entreprise française pour l'hebergement de serveur.",
+        path:  'https://www.ovhcloud.com/fr/'
+    },
+    {
+        title: 'PlanetHoster',
+        description: "Service d'hébergement de site web.",
+        path:  'https://www.planethoster.com/fr'
+    },
 ];
