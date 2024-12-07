12d8e946f1514fc9366efe4cd80cf92238755da2
Aline Leroy <aline.leroy.27@gmail.com>
2024-12-07 22:27:33 +0100

    create a coordinates file, add path to general card

---
+++ src/datas/coordinatesCities.ts
@@ -0,0 +1,22 @@
+export const coordinatesCities = [
+    {
+        city: "Lille",
+        coordinates: [50.6310514, 2.9643256]
+    },
+    {
+        city: "Lyon",
+        coordinates: [45.757921, 4.7525576]
+    },
+    {
+        city: "Paris",
+        coordinates: [48.8588254, 2.2644627]
+    },
+    {
+        city: "Nantes",
+        coordinates: [47.2382031, -1.6429069]
+    },
+    {
+        city: "Grenoble",
+        coordinates: [45.188529, 5.724524]
+    }
+];
