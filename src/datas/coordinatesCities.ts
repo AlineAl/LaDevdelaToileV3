817341c8c11e9d11e6d0fbcc40ed53bf74680f43
Aline Leroy <aline.leroy.27@gmail.com>
2024-12-08 22:31:46 +0100

    add a function to generate general path meetup with event title, add filter by select and input

--- src/datas/coordinatesCities.ts
+++ src/datas/coordinatesCities.ts
@@ -1,17 +1,5 @@
 export const coordinatesCities = [
     {
-        city: "Lille",
-        coordinates: [50.6310514, 2.9643256]
-    },
-    {
-        city: "Lyon",
-        coordinates: [45.757921, 4.7525576]
-    },
-    {
-        city: "Paris",
-        coordinates: [48.8588254, 2.2644627]
-    },
-    {
         city: "Nantes",
         coordinates: [47.2382031, -1.6429069]
     },
