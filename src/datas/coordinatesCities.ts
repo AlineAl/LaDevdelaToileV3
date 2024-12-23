83de34e4bfeffea453e6b377db52961b2f130788
Amelie Benoit <amelie.benoit@busbud.com>
2024-12-23 10:55:00 +0100

    chore(cities): add Bordeux

--- src/datas/coordinatesCities.ts
+++ src/datas/coordinatesCities.ts
@@ -10,5 +10,9 @@ export const coordinatesCities = [
     {
         city: "Lyon",
         coordinates: [45.7578137, 4.8320114]
+    },
+    {
+        city: "Bordeaux",
+        coordinates: [44.833328, -0.56667]
     }
 ];
