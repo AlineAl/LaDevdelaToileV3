cf9dd5e26d8c67f9b9041deae3d4e283f5482554
Aline Leroy <aline.leroy.27@gmail.com>
2024-12-29 01:14:17 +0100

    remove note path in navbar and add filter tag to list share ressource

--- src/datas/books.ts
+++ src/datas/books.ts
@@ -4,6 +4,57 @@ export const books = [
         author: "Barbara Oakley et Olav Schewe",
         description:
             "Ce livre vous donnera des clés pour apprendre plus vite et plus efficacement, en vous appuyant sur les dernières découvertes en neurosciences.",
-        path: "https://www.babelio.com/livres/Oakley-Apprendre-vite-et-bien/1389150"
+        path: "https://www.babelio.com/livres/Oakley-Apprendre-vite-et-bien/1389150",
+        img: "https://m.media-amazon.com/images/I/41r1O2eSIPL._SX195_.jpg",
+        tags: ["apprentissage", "efficacité", "neurosciences", "développement personnel", "méthodes d'étude"]
+    },
+    {
+        title: "Le talent est une fiction",
+        author: "Samah Karaki",
+        description:
+            "Si vous souhaitez comprendre plus en profondeur comment nous fonctionnons et surtout remettre en question les notions de talent, de réussite et d'intelligence, je vous conseille fortement cette lecture !",
+        path: "https://www.babelio.com/livres/Karaki-Le-talent-est-une-fiction/1437668",
+        img: "https://m.media-amazon.com/images/I/41gu1yY5U3L._SX195_.jpg",
+        tags: ["psychologie", "talent", "intelligence", "réussite", "développement personnel"]
+    },
+    {
+        title: "Mathematica",
+        author: "David Bessis",
+        description: "Contrairement aux idées reçues, les mathématiques ne sont pas réservées à une élite. Ce livre démontre que le talent inné n'existe pas. Tout est question de comment apprendre à apprendre et de méthode.",
+        path: "https://www.babelio.com/livres/Bessis-Mathematica/1392922",
+        img: "https://www.babelio.com/couv/CVT_Mathematica_3867.jpg",
+        tags: ["mathématiques", "apprentissage", "méthodologie", "science", "pédagogie"]
+    },
+    {
+        title: "Coder proprement",
+        author: "Robert C. Martin",
+        description: "Un des livres les plus cité et partagé quand on se lance dans le développement web. Bien qu'ayant quelques années, il vous donnera des clés pour écrire un code propre, lisible et maintenable.",
+        path: "https://www.amazon.fr/Coder-proprement-Robert-C-Martin/dp/232600227X",
+        img: "https://m.media-amazon.com/images/I/91rDX5YuLVL._SL1500_.jpg",
+        tags: ["développement logiciel", "code propre", "programmation", "bonnes pratiques", "développement web"]
+    },
+    {
+        title: "Learning Domain-Driven Design",
+        author: "Vlad Khononov",
+        description: "En explorant la relation entre le Domain-Driven Design (DDD) et d'autres méthodologies, l'auteur illustre comment ces pratiques favorisent une architecture logicielle robuste et adaptée à l'évolution des exigences.",
+        path: "https://www.amazon.fr/Learning-Domain-driven-Design-Aligning-Architecture/dp/1098100131",
+        img: "https://m.media-amazon.com/images/I/819gVKYN7HL._SL1500_.jpg",
+        tags: ["Domain-Driven Design", "DDD", "architecture logicielle", "modélisation", "méthodologies"]
+    },
+    {
+        title: "Domain-Driven Design",
+        author: "Eric Evans",
+        description: "À travers quatre parties, le livre explore les objectifs du DDD, les bonnes pratiques de modélisation orientée objet, les principes de refactorisation et les stratégies de conception à grande échelle, illustrés par des exemples réalistes issus de projets concrets.",
+        path:"https://www.amazon.fr/Domain-Driven-Design-Tackling-Complexity-Software/dp/0321125215",
+        img: "https://m.media-amazon.com/images/I/81ykBjVaUjL._SL1500_.jpg",
+        tags: ["Domain-Driven Design", "DDD", "architecture logicielle", "modélisation", "refactorisation"]
+    },
+    {
+        title: "Computer Science Distilled",
+        author: "Wladston Ferreira Filho",
+        description: "Ce livre couvre les bases des mathématiques, les algorithmes et structures de données courants, ainsi que les principes fondamentaux du fonctionnement des ordinateurs et des langages de programmation.",
+        path: "https://www.amazon.com/Computer-Science-Distilled-Computational-Problems/dp/0997316020",
+        img: "https://m.media-amazon.com/images/I/7150iLi82cL._SL1360_.jpg",
+        tags: ["science informatique", "algorithmes", "structures de données", "programmation", "bases"]
     }
 ];
---
+++ src/datas/tags.ts
@@ -0,0 +1,72 @@
+import type { TagCategory } from "../utils/types/TagCategory.ts";
+export const tagCategories: TagCategory[] = [
+    {
+        learning: {
+            color: "#155e75",
+            tags: [
+                "apprentissage",
+                "efficacité",
+                "neurosciences",
+                "pédagogie",
+                "méthodologie"
+            ]
+        }
+    },
+    {
+        psychology: {
+            color: "#6b21a8",
+            tags: [
+                "développement personnel",
+                "psychologie",
+                "talent",
+                "intelligence",
+                "réussite"
+            ]
+        }
+    },
+    {
+        science: {
+            color: "#075985",
+            tags: [
+                "mathématiques",
+                "science"
+            ]
+        }
+    },
+    {
+        softwareDevelopment: {
+            color: "#9d174d",
+            tags: [
+                "développement logiciel",
+                "code propre",
+                "programmation",
+                "bonnes pratiques",
+                "développement web"
+            ]
+        }
+    },
+    {
+        architecture: {
+            color: "#701a75",
+            tags: [
+                "Domain-Driven Design",
+                "DDD",
+                "architecture logicielle",
+                "modélisation",
+                "refactorisation"
+            ]
+        }
+    },
+    {
+        computerScience: {
+            color: "#eab308",
+            tags: [
+                "science informatique",
+                "algorithmes",
+                "structures de données",
+                "bases",
+                "langages de programmation"
+            ]
+        }
+    }
+];
